0c86118f9b1cef86625bfc51f866ca240d56fbe0
mengxuyiGit <mengxuyiabc@gmail.com>
2024-09-22 21:07:52 +0000

    resume training stage2 w/o ROPE from 20k zeroSNR

--- run_train_stage2.sh
+++ run_train_stage2.sh
@@ -1,5 +1,7 @@
 # stage 2
 # export CUDA_LAUNCH_BLOCKING=1
 # export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=6,7
 # accelerate launch --config_file 1gpu.yaml train_mvdiffusion_joint.py --config configs/train/stage2-joint-6views-lvis.yaml
-accelerate launch --config_file 1gpu.yaml train_mvdiffusion_joint_splatter.py --config configs/train/stage2-joint-6views-lvis.yaml
+# accelerate launch --config_file 1gpu.yaml train_mvdiffusion_joint_splatter.py --config configs/train/stage2-joint-6views-lvis.yaml
+accelerate launch --config_file 2gpu.yaml train_mvdiffusion_joint_splatter.py --config configs/train/stage2-joint-6views-lvis-render_2dgs.yaml
