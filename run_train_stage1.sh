a4ee10818a0d84c23a02055510cacf5e41a9af21
mengxuyiGit <mengxuyiabc@gmail.com>
2025-07-19 15:12:33 +0000

    config & script: stage1 training on 4x256x256 for (tmlr ablation)

--- run_train_stage1.sh
+++ run_train_stage1.sh
@@ -15,7 +15,9 @@ export CUDA_LAUNCH_BLOCKING=1
 ## 6 views x128x128
 # accelerate launch --config_file 4gpu.yaml train_mvdiffusion_image.py --config configs/train/stage1-mix-6views-lara_camNormFalse-tmlr_6x128x128.yaml
 ## 4 views x128x128
-accelerate launch --config_file 4gpu.yaml train_mvdiffusion_image.py --config configs/train/stage1-mix-6views-lara_camNormFalse-tmlr_4x128x128.yaml
+# accelerate launch --config_file 4gpu.yaml train_mvdiffusion_image.py --config configs/train/stage1-mix-6views-lara_camNormFalse-tmlr_4x128x128.yaml
+## 4 views x256x256
+accelerate launch --config_file 4gpu.yaml train_mvdiffusion_image.py --config configs/train/stage1-mix-6views-lara_camNormFalse-tmlr_4x256x256.yaml
 
 
 ### debug
